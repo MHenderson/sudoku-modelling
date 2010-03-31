3380ebaf1da66e2f63982c698a2904323bd2c6a4
Matthew Henderson <matthew.james.henderson@gmail.com>
2010-03-31 19:18:37 -0400

    Shidoku demo created.

--- src/constraint.py
+++ src/constraint.py
@@ -1,6 +1,6 @@
 # Sean Davis, Andrew Smith (Berea) 3.31.2010
 
-import constraint
+from constraint import *
 
 def printSolutions(problem):
     solution = problem.getSolutions()
@@ -11,3 +11,22 @@ def printSolutions(problem):
 def n_solutions(problem):
     solution = problem.getSolutions()
     return len(solution)
+
+def shidoku_test():
+    shidoku = Problem()
+    shidoku.addVariables(range(1,16 + 1),range(1,4 + 1))
+    shidoku.addConstraint(AllDifferentConstraint(),[1,2,3,4])
+    shidoku.addConstraint(AllDifferentConstraint(),[5,6,7,8])
+    shidoku.addConstraint(AllDifferentConstraint(),[9,10,11,12])
+    shidoku.addConstraint(AllDifferentConstraint(),[13,14,15,16])
+    shidoku.addConstraint(AllDifferentConstraint(),[1,5,9,13])
+    shidoku.addConstraint(AllDifferentConstraint(),[2,6,10,14])
+    shidoku.addConstraint(AllDifferentConstraint(),[3,7,11,15])
+    shidoku.addConstraint(AllDifferentConstraint(),[4,8,12,16])
+    shidoku.addConstraint(AllDifferentConstraint(),[1,2,5,6])
+    shidoku.addConstraint(AllDifferentConstraint(),[3,4,7,8])
+    shidoku.addConstraint(AllDifferentConstraint(),[9,10,13,14])
+    shidoku.addConstraint(AllDifferentConstraint(),[11,12,15,16])
+    shidoku.addConstraint(ExactSumConstraint(1),[7])
+    return shidoku
+
