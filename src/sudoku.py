377e72d34302bb085877dcd118d725ade81a7f27
bluesabre_davis <daviss@berea.edu>
2010-05-02 19:33:48 -0400

    Updated documentation to Python standard.

--- src/sudoku.py
+++ src/sudoku.py
@@ -7,8 +7,9 @@ import itertools
 import networkx
 
 def convert_to_sage(number_string):
-    """Conversion of Sudoku puzzle strings.
-    
+    """convert_to_sage(number_string) -> string
+
+    Returns a converted sudoku puzzle string.
     After conversion an empty cell is represented by period instead of 0."""
     return number_string.replace('0','.')
 
@@ -21,43 +22,60 @@ def cells(boxsize): return range(1, n_cells(boxsize) + 1)
 def symbols(boxsize): return range(1, n_rows(boxsize) + 1)
 
 def top_left_cells(boxsize): 
-    """List of cell labels of top-left cell of each box."""
+    """top_left_cells(boxsize) -> list
+
+    Returns a list of cell labels of the top-left cell of each box."""
     return [cell(i,j,boxsize) for i in range(1,n_rows(boxsize),boxsize) for j in range(1,n_cols(boxsize),boxsize)]
 
 def rows(boxsize):
-    """Cell labels ordered by row."""
+    """rows(boxsize) -> list
+
+    Returns a list of cell labels ordered by row for the given boxsize."""
     nr = n_rows(boxsize)
     return [range(nr*(i-1)+1,nr*i+1) for i in range(1,nr+1)]
 
 def cols(boxsize):
-    """Cell labels ordered by column."""
+    """cols(boxsize) -> list
+
+    Returns a list of cell labels ordered by column for the given boxsize."""
     nc = n_cols(boxsize)
     ncl = n_cells(boxsize)
     return [range(i,ncl+1-(nc-i),nc) for i in range(1,nc+1)]
 
 def boxes(boxsize):
-    """Cell labels ordered by box."""
+    """boxes(boxsize) -> list
+
+    Returns a list of cell labels ordered by box for the given boxsize."""
     nr = n_rows(boxsize)
     nc = n_cols(boxsize)
     return [[i+j+k for j in range(0,boxsize*nr,nc) for k in range(0,boxsize)] for i in top_left_cells(boxsize)]
 
 def add_row_constraints(problem, boxsize):
-    """Add to constraint problem 'problem', all_different constraints on rows."""
+    """add_row_constraints(problem, boxsize)
+
+    Adds to constraint problem 'problem', all_different constraints on rows."""
     for row in rows(boxsize):
         problem.addConstraint(AllDifferentConstraint(), row)
 
 def add_col_constraints(problem, boxsize):
-    """Add to constraint problem 'problem', all_different constraints on columns."""
+    """add_col_constraints(problem, boxsize)
+
+    Adds to constraint problem 'problem', all_different constraints on columns."""
     for col in cols(boxsize):    
         problem.addConstraint(AllDifferentConstraint(), col)
 
 def add_box_constraints(problem, boxsize):
-    """Add to constraint problem 'problem', all_different constraints on boxes."""
+    """add_box_constraints(problem, boxsize)
+
+    Adds to constraint problem 'problem', all_different constraints on boxes."""
     for box in boxes(boxsize):
         problem.addConstraint(AllDifferentConstraint(), box)
 
 def empty_puzzle(boxsize):
-    """Create a constraint problem representing an empty Sudoku puzzle of box-dimension 'boxsize'."""
+    """empty_puzzle(boxsize) -> constraint.Problem
+
+    Returns a constraint problem representing an empty Sudoku puzzle of 
+    box-dimension 'boxsize'."""
     p = Problem()
     p.addVariables(cells(boxsize), symbols(boxsize)) 
     add_row_constraints(p, boxsize)
@@ -66,8 +84,10 @@ def empty_puzzle(boxsize):
     return p
 
 def puzzle(boxsize, clues):
-    """Create a constraint problem representing a Sudoku puzzle, where the fixed
-       cells are specified by 'clues' dictionary."""
+    """puzzle(boxsize, clues) -> constraint.Problem
+
+    Returns a constraint problem representing a Sudoku puzzle, where the fixed
+    cells are specified by 'clues' dictionary."""
     p = empty_puzzle(boxsize)
     for x in range(1, len(clues)+1):
         if clues[x] != 0:
@@ -75,8 +95,10 @@ def puzzle(boxsize, clues):
     return p
 
 def random_puzzle(boxsize, solution, fixed):
-    """From 'solution' dictionary, generate a random Sudoku puzzle with 'fixed'
-       number of cells."""
+    """random_puzzle(boxsize, solution, fixed) -> constraint.Problem
+
+    Returns a constraint problem representing a random Sudoku puzzle of 'fixed' 
+    size from the 'solution' dictionary."""
     indices = []
     for x in range(1, len(solution)+1):
         indices.append(x)
@@ -87,14 +109,19 @@ def random_puzzle(boxsize, solution, fixed):
     return puzzle(boxsize, solution)
 	
 def dict_to_sudoku_string(solution):
-    """Conversion from dictionary to puzzle string."""
+    """dict_to_sudoku_string(solution) -> string
+
+    Returns a puzzle string converted from the 'solution' dictionary."""
     string = ""
     for x in range(1, len(solution)+1):
         string = string + str(solution[x])
     return string
 
 def make_sudoku_constraint(number_string, boxsize):
-    """Create constraint problem from 'number_string' Sudoku puzzle string."""
+    """make_sudoku_constraint(number_string, boxsize) -> constraint.Problem
+
+    Returns a constraint problem representing a Sudoku puzzle from the 
+    'number_string' Sudoku puzzle string."""
     p = empty_puzzle(boxsize)
     for x in range(n_cells(boxsize)):
         if number_string[x] != "0":
@@ -102,19 +129,27 @@ def make_sudoku_constraint(number_string, boxsize):
     return p
 	
 def list_to_string(list):
-    """Implemented since the dancing links algorithm returns a list."""
+    """list_to_string(list) -> string
+
+    Returns the string representation of 'list'.
+    Implemented since the dancing links algorithm returns a list."""
     output = ""
     for i in range(len(list)):
         output += str(list[i])
     return output
 
 def process_puzzle(puzzle, boxsize):
-    """Constraint processing strategy."""
+    """process_puzzle(puzzle, boxsize) -> string
+
+    Returns a solved Sudoku puzzle string from the Sudoku string 'puzzle'.
+    Constraint processing strategy."""
     p = make_sudoku_constraint(puzzle, boxsize)
     return dict_to_sudoku_string(p.getSolution())
 
 def solve_from_file(infile, outfile, boxsize):
-    """Gets all solutions for all items provided in a file."""
+    """solve_from_file(infile, outfile, boxsize)
+
+    Outputs solutions to puzzles in file 'infile' to file 'outfile'."""
     input = open(infile, 'r')
     output = open(outfile, 'w')
     puzzles = input.readlines()
@@ -123,11 +158,15 @@ def solve_from_file(infile, outfile, boxsize):
         output.write(s + "\n")
 
 def add_all_edges(graph, vertices):
-    """Adds all edges between nodes in 'vertices' to 'graph'."""
+    """add_all_edges(graph, vertices)
+
+    Adds all edges between nodes in 'vertices' to 'graph'."""
     graph.add_edges_from(itertools.combinations(vertices, 2))
 
 def empty_sudoku_graph(boxsize):
-    """Create the Sudoku graph of dimension 'boxsize'."""
+    """empty_sudoku_graph(boxsize) -> networkx.Graph
+
+    Returns the Sudoku graph of dimension 'boxsize'."""
     g = networkx.Graph()
     g.add_nodes_from(cells(boxsize))
     for vertices in rows(boxsize):
