612be3be9712adce62d2401d9bf4e4e664b556a8
Matthew Henderson <matthew.james.henderson@gmail.com>
2010-06-25 10:15:45 -0400

    Enumeration options added to file-processing script.

--- scripts/solve_sudoku.py
+++ scripts/solve_sudoku.py
@@ -1,53 +1,66 @@
 #!/usr/bin/python
 
+from __future__ import print_function
+
 import sudoku, sys, progressbar, math, getopt
 
 def usage():
-    print "Usage: "
+    print("Usage: ")
 
-def run(infilename, output, boxsize, model, verbose = False):
+def run(infilename, output, boxsize, model, verbose = False, verify = False, count = False):
 
     puzzles = open(infilename, 'r')
-    solutions = open(output, 'a')
+    outfile = open(output, 'a')
     puzzles_d = map(lambda puzzle:sudoku.string_to_dict(puzzle, boxsize), puzzles)
 
-    s = []
+    solutions = []
 
-    p = progressbar.ProgressBar()
+    pbar = progressbar.ProgressBar()
 
     n_puzzles = 0
 
     for puzzle in puzzles_d:
-        s.append(sudoku.solve(puzzle, boxsize, model))
+        p = sudoku.Puzzle(puzzle, boxsize)        
+        if count:
+            s = sudoku.well_formed_solution(p)
+        else:
+            s = sudoku.solve(p, model)
+        solutions.append(s)
         if verbose:
             n_puzzles += 1
             percentage = 1.0 * n_puzzles/len(puzzles_d) * 100
             int_percentage = int(math.ceil(percentage))
-            p.render(int_percentage)
-
-    if sudoku.verify_solutions(puzzles, s, boxsize, verify_solution = sudoku.is_sudoku_solution_s):
-        sudoku.print_puzzles(s, boxsize, file = solutions)
-    else:
-        pass
-
+            pbar.render(int_percentage)
+        if verify:
+            if not sudoku.is_sudoku_solution(p, s):
+                print("Non-solution")
+                sys.exit(2)
+    print(s, file = outfile)
+    
     puzzles.close()
-    solutions.close()
+    outfile.close()
 
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hvo:b:m:")
+        opts, args = getopt.getopt(sys.argv[1:], "hvcwo:b:m:")
     except getopt.GetoptError, err:
-        print str(err)
+        print(str(err))
         usage()
         sys.exit(2)
     output = None
     verbose = False
+    verify = False
+    count = False
     model = 'CP'
     boxsize = 3
     for o, a in opts:
         if o == "-v":
             verbose = True
+        elif o == "-c":
+            verify = True
+        elif o == "-w":
+            count = True
         elif o in ("-h", "--help"):
             usage()
             sys.exit()
@@ -59,7 +72,7 @@ def main():
             model = a
         else:
             assert False, "unhandled option"
-    run(args[0], output, boxsize, model, verbose)
+    run(args[0], output, boxsize, model, verbose, verify, count)
 
 if __name__ == "__main__":
     main()
