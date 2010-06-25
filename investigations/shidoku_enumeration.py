e0510ab418110da5fe76b12bf6b1496d26af23f0
Matthew Henderson <matthew.james.henderson@gmail.com>
2010-06-25 10:16:25 -0400

    Updated Shidoku enumeration investigation script.

--- investigations/shidoku_enumeration.py
+++ investigations/shidoku_enumeration.py
@@ -1,11 +1,8 @@
 from timeit import Timer
 
 if __name__ == "__main__":
-    setup_string = "from sudoku import empty_puzzle_as_CP"
-    experiment_string = """\
-p = empty_puzzle_as_CP(2)
-s = p.getSolutions()
-print len(s)"""
+    setup_string = "from sudoku import Puzzle, count_solutions"
+    experiment_string = """print count_solutions(Puzzle({},2))"""
     t = Timer(experiment_string, setup_string)
     print t.timeit(number=1)
 
